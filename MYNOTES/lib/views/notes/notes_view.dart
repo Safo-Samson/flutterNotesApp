d364f22d353acc2e596ae7e30824554e41cd803a
safo samson <safosamson18@gmail.com>
2024-01-15 17:54:10 +0000

    minior

--- MYNOTES/lib/views/notes/notes_view.dart
+++ MYNOTES/lib/views/notes/notes_view.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/enums/menu_actions.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
 import 'package:mynotes/services/cloud/cloud_note.dart';
 import 'package:mynotes/services/cloud/firebase_cloud_storage.dart';
 import 'package:mynotes/views/notes/notes_list_view.dart';
@@ -48,9 +51,10 @@ class _NotesViewState extends State<NotesView> {
                   final result = await showLogOutDialog(context);
 
                   if (result) {
-                    await AuthService.firebase().logOut();
-                    Navigator.of(context)
-                        .pushNamedAndRemoveUntil(loginRoute, (_) => false);
+                    context.read<AuthBloc>().add(const AuthEventLogOut());
+                    // no needed because the bloc will handle the navigation
+                    // Navigator.of(context)
+                    //     .pushNamedAndRemoveUntil(loginRoute, (_) => false);
                   }
                   break;
               }
